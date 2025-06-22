8b03b3a7aa3e60074fd41589493635f6a3cbf35e
ayushSH-ze <ayushash1619@gmail.com>
2025-06-22 13:52:15 +0530

    made some changes

--- factorial.java
+++ factorial.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class factorial {
 
-    public static int fact(int num){
+    public static void fact(int num){
         int f = 1;
+
+        if(num<0){
+            System.out.println("Invalid number");
+            return;
+        }
+
         for(int x=2; x<=num; x++){
             f *= x;
         }
-        return f;
+        
+        System.out.printf("factoria of %d = %d", num, f);
     }
     public static void main(String[] args) {
         System.out.print("Enter a number: ");
@@ -16,8 +23,8 @@ public class factorial {
 
         int num = sc.nextInt();
         sc.close();
+
+        fact(num);
         
-        int res = fact(num);
-        System.out.printf("factoria of %d = %d", num, res);
     }
 }
